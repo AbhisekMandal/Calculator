0376bc9ccaf8ae107a0f38eb41bcfae9479f656f
Abhisek <mandalabhisek20@gmail.com>
2025-04-03 10:51:48 +0530

    catch html elements by Id on JS file

---
+++ index.js
@@ -0,0 +1,7 @@
+let num1=document.getElementById("num1");
+let num2=document.getElementById("num2");
+let add=document.getElementById("add");
+let sub=document.getElementById("sub");
+let into=document.getElementById("into");
+let div=document.getElementById("div");
+let ans=document.getElementById("ans");
